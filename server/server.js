c6649cca2173fc1fc5850e193a51b3085583d6d2
Ricky Rivas <rickyrivas918@gmail.com>
2021-06-22 01:07:09 -0500

    /

--- server/server.js
+++ server/server.js
@@ -4,8 +4,27 @@ const express = require("express");
 const app = express();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-(async () => {
-  console.log(await stripe.plans.list());
-})();
+app.post("/create-checkout-session", async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items: [
+      {
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name: "T-shirt",
+          },
+          unit_amount: 2000,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: "payment",
+    success_url: "https://example.com/success",
+    cancel_url: "https://example.com/cancel",
+  });
 
-app.listen(4000, () => console.log("successfully done, Ricky"));
+  res.json({ id: session.id });
+});
+
+app.listen(3000, () => console.log("successfully done, Ricky"));
