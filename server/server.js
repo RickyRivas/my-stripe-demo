4840b3a41d0b19c620210fe5317bd895ff87ac0e
Ricky Rivas <rickyrivas918@gmail.com>
2021-06-22 01:00:23 -0500

    .

--- server/server.js
+++ server/server.js
@@ -3,17 +3,9 @@ require("dotenv").config({ path: ".env" });
 const express = require("express");
 const app = express();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-const { resolve } = require("path");
 
 (async () => {
   console.log(await stripe.plans.list());
 })();
 
-app.use(express.static("/client"));
-
-app.get("/", (req, res) => {
-  const path = resolve("/client" + "/index.html");
-  res.sendFile(path);
-});
-
-app.listen(4242, () => console.log("successfully done, Ricky"));
+app.listen(4000, () => console.log("successfully done, Ricky"));
