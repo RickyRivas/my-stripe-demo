a1bcbe7458171689e1ab0db4d6a3bcdc57eecc60
Ricky Rivas <rickyrivas918@gmail.com>
2021-06-22 23:01:58 -0500

    .

--- server.js
+++ server.js
@@ -9,18 +9,7 @@ app.use(express.static("public"));
 app.post("/create-checkout-session", async (req, res) => {
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
-    line_items: [
-      {
-        price_data: {
-          currency: "usd",
-          product_data: {
-            name: "T-shirt",
-          },
-          unit_amount: 2000,
-        },
-        quantity: 1,
-      },
-    ],
+    line_items: [],
     mode: "payment",
     success_url: "https://example.com/success",
     cancel_url: "https://example.com/cancel",
